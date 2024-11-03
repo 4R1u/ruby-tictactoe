54480a2f1765e497298eafa4ae39e2c94e81c316
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-11-03 16:33:08 +0400

    Add function to Input Integer from 0 to 2

--- main.rb
+++ main.rb
@@ -5,7 +5,9 @@ require './lib/board'
 
 board = Board
 
-user_wants_to_quit = false
-
-until user_wants_to_quit
+def input_coord
+  row = 3
+  puts 'A value from 0 to 2'
+  row = gets.chomp.to_i until (0..2).cover?(row)
+  row
 end
