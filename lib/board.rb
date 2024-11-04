a970dc897f644bdd8100538264c16e3f7ab11334
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-11-04 11:46:21 +0400

    Fix Board#initialize by Making All Default Values Unique

--- lib/board.rb
+++ lib/board.rb
@@ -5,7 +5,7 @@ require_relative 'spot'
 # Represents the board on which the game will be played.
 class Board
   def initialize
-    @grid = Array.new(3) { Array.new(3, Spot.new) }
+    @grid = Array.new(3) { Array.new(3) { Spot.new } }
   end
 
   def view
