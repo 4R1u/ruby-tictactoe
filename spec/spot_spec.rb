bff911fcaae4a633119388c6c5f0f6135a688d22
4R1u <62248461+4R1u@users.noreply.github.com>
2024-12-30 15:52:30 +0400

    Add tests for Spot#mark_o

--- spec/spot_spec.rb
+++ spec/spot_spec.rb
@@ -16,4 +16,17 @@ describe Spot do
       expect(spot.mark).not_to eq(:X)
     end
   end
+
+  describe '#mark_o' do
+    it 'changes mark when mark is empty' do
+      spot.mark_o
+      expect(spot.mark).to eq(:O)
+    end
+
+    it "doesn't change when the spot has already been marked" do
+      spot.mark_x
+      spot.mark_o
+      expect(spot.mark).not_to eq(:O)
+    end
+  end
 end
