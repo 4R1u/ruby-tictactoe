ee69d76d6a2a4c76289d51cb7525433141487156
4R1u <62248461+4R1u@users.noreply.github.com>
2024-12-31 10:53:57 +0400

    test: Board#winner? when O wins

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -88,15 +88,87 @@ describe Board do
       end
     end
 
-    # context 'when O wins 1-2-3' do
-    # context 'when O wins 4-5-6' do
-    # context 'when O wins 7-8-9' do
+    context 'when O wins 1-2-3' do
+      before do
+        spot.mark_o(0, 0)
+        spot.mark_o(0, 1)
+        spot.mark_o(0, 2)
+      end
+      it 'O wins' do
+        expect(spot.winner?(:O)).to be(true)
+      end
+    end
+    context 'when X wins 4-5-6' do
+      before do
+        spot.mark_o(1, 0)
+        spot.mark_o(1, 1)
+        spot.mark_o(1, 2)
+      end
+      it 'X wins' do
+        expect(spot.winner?(:O)).to be(true)
+      end
+    end
+    context 'when O wins 7-8-9' do
+      before do
+        spot.mark_o(2, 0)
+        spot.mark_o(2, 1)
+        spot.mark_o(2, 2)
+      end
+      it 'O wins' do
+        expect(spot.winner?(:O)).to be(true)
+      end
+    end
 
-    # context 'when O wins 1-4-7' do
-    # context 'when O wins 2-5-8' do
-    # context 'when O wins 3-6-9' do
+    context 'when O wins 1-4-7' do
+      before do
+        spot.mark_o(0, 0)
+        spot.mark_o(1, 0)
+        spot.mark_o(2, 0)
+      end
+      it 'O wins' do
+        expect(spot.winner?(:O)).to be(true)
+      end
+    end
+    context 'when O wins 2-5-8' do
+      before do
+        spot.mark_o(0, 1)
+        spot.mark_o(1, 1)
+        spot.mark_o(2, 1)
+      end
+      it 'O wins' do
+        expect(spot.winner?(:O)).to be(true)
+      end
+    end
+    context 'when O wins 3-6-9' do
+      before do
+        spot.mark_o(0, 2)
+        spot.mark_o(1, 2)
+        spot.mark_o(2, 2)
+      end
+      it 'O wins' do
+        expect(spot.winner?(:O)).to be(true)
+      end
+    end
 
-    # context 'when O wins 1-5-9' do
-    # context 'when O wins 7-5-3' do
+    context 'when O wins 1-5-9' do
+      before do
+        spot.mark_o(0, 0)
+        spot.mark_o(1, 1)
+        spot.mark_o(2, 2)
+      end
+      it 'O wins' do
+        expect(spot.winner?(:O)).to be(true)
+      end
+    end
+    context 'when O wins 3-5-7' do
+      before do
+        spot.mark_o(0, 2)
+        spot.mark_o(1, 1)
+        spot.mark_o(2, 0)
+      end
+      it 'O wins' do
+        expect(spot.winner?(:O)).to be(true)
+      end
+    end
   end
 end
