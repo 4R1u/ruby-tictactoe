74631646592e5125d765f91f83c4e103079924a2
4R1u <62248461+4R1u@users.noreply.github.com>
2024-12-31 08:55:34 +0400

    test: Accomplish splitting Spot#mark into contexts

--- spec/spot_spec.rb
+++ spec/spot_spec.rb
@@ -6,15 +6,19 @@ require 'colorize'
 describe Spot do
   subject(:spot) { described_class.new }
   describe '#mark_x' do
-    it 'changes mark when mark is empty' do
-      spot.mark_x
-      expect(spot.mark).to eq(:X)
+    context 'when mark is empty' do
+      it 'changes mark' do
+        spot.mark_x
+        expect(spot.mark).to eq(:X)
+      end
     end
 
-    it "doesn't change when the spot has already been marked" do
-      spot.mark_o
-      spot.mark_x
-      expect(spot.mark).not_to eq(:X)
+    context 'when the spot has already been marked' do
+      it "doesn't change when the spot has already been marked" do
+        spot.mark_o
+        spot.mark_x
+        expect(spot.mark).not_to eq(:X)
+      end
     end
   end
 
